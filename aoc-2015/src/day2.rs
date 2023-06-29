f49b3e866f041bf5abbdabbb28abd5399a7ba636
patrickmilnes <pjamilnes@gmail.com>
2023-06-29 18:46:00 +0100

    Completed day 2 part 1

--- aoc-2015/src/day2.rs
+++ aoc-2015/src/day2.rs
@@ -1,5 +1,32 @@
 fn part_1(input: &str) -> i32 {
-    33
+    let dimention_list: Vec<&str> = input.lines().collect();
+
+    let mut total_feet = 0;
+
+    for dimention in dimention_list {
+        let mut split = dimention.trim().split('x');
+        // L x W x H
+        let l: i32 = split.next().unwrap().parse().unwrap();
+        let w: i32 = split.next().unwrap().parse().unwrap();
+        let h: i32 = split.next().unwrap().parse().unwrap();
+
+        let init_area_1 = l * w;
+        let init_area_2 = w * h;
+        let init_area_3 = h * l;
+
+        let areas = vec![init_area_1, init_area_2, init_area_2];
+        let min = areas.iter().min().unwrap();
+
+        let area_1 = 2 * init_area_1;
+        let area_2 = 2 * init_area_2;
+        let area_3 = 2 * init_area_3;
+
+        let total = area_1 + area_2 + area_3 + min;
+
+        total_feet += total;
+    }
+
+    total_feet
 }
 
 fn part_2(input: &str) -> i32 {
@@ -14,14 +41,14 @@ mod day2_tests {
 
     #[test]
     fn part_1_test() {
-        let input = get_input("./inputs/data1.txt");
+        let input = get_input("./inputs/data2.txt");
         let r = part_1(&input);
-        assert_eq!(r, 33);
+        assert_eq!(r, 1602437);
     }
 
     #[test]
     fn part_2_test() {
-        let input = get_input("./inputs/data1.txt");
+        let input = get_input("./inputs/data2.txt");
         let r = part_2(&input);
         assert_eq!(r, 33);
     }
